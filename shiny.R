d03eae46f7b546703356563997a6358768d3bdbd
rsm-sal072 <sal072@ucsd.edu>
2024-02-02 15:16:58 -0800

    02022024

---
+++ shiny.R
@@ -0,0 +1,73 @@
+library(shiny)
+library(shinythemes)
+library(shinyWidgets)
+library(DT)
+library(rmarkdown)
+library(shinyjs)
+
+ui <- fluidPage(
+  
+  id = "main_nav",
+  title = (""),
+  tabPanel("",
+           fluidPage(
+             pickerInput("chapter", "",
+                         choices = c("People Analytics, Sample", 
+                                     "Note")),
+             uiOutput("chapter"))),
+  
+  
+tags$style(HTML("
+    #logo {
+      position: fixed;
+      right: 20px;
+      bottom: 20px;  # Add some space at the bottom
+    }
+  ")),
+
+  tags$img(id="logo", src="logo.png"),
+  
+  tags$div(style = "height: 20px;") 
+  
+)
+
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+
+
+
+server <- function(input, output, session) {
+  active_tab <- reactiveVal("chapter")
+  
+  
+  ################################## Chapter 1: Descision Analysis ###########################################
+  
+  
+  output$chapter <- renderUI({
+    req(input$chapter) 
+    
+    file_name <- switch(input$chapter,
+                        "People Analytics, Sample" = "logit_ventura.html",
+                        "Note" = "page1.html")
+    
+    if (!is.null(file_name)) {
+      tags$iframe(src = file_name, style = "width:100%; height:800px;")
+    }
+  })
+  
+  observeEvent(input$main_nav, {
+    active_tab(input$main_nav)
+  })
+  
+  
+  #########################################################################################################################################################
+  
+  
+}
+
+shinyApp(ui = ui, server = server)
+
+
